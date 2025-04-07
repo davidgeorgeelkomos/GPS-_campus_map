958f4b85fa6aeb4f9ef2146850588bae80051812
Arsany Samuel <arsanysamuel.as@gmail.com>
2025-04-07 13:10:38 +0200

    template edits

--- static/js/main.js
+++ static/js/main.js
@@ -18,3 +18,23 @@ function onLocationFound(e) {
 }
 
 map.on('locationfound', onLocationFound);
+const mousePositionControl = L.control({ position: 'bottomleft' });
+ mousePositionControl.onAdd = function() {
+            const div = L.DomUtil.create('div', 'leaflet-control-mouseposition');
+            div.innerHTML = 'Lat: , Lng: ';
+            return div;
+        };
+
+        // Add the custom control to the map
+        mousePositionControl.addTo(map);
+
+        // Update the mouse position on the map when it moves
+        map.on('mousemove', function(e) {
+            const lat = e.latlng.lat.toFixed(4); // Get latitude and round it
+            const lng = e.latlng.lng.toFixed(4); // Get longitude and round it
+            mousePositionControl.getContainer().innerHTML = `Lat: ${lat}, Lng: ${lng}`;
+        });
+
+var imageUrl = 'static/images/tst_home.png',
+    imageBounds = [[30.0513, 31.2424], [30.0474, 31.2466]];
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
